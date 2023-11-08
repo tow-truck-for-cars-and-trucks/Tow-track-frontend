055051a252914da0f1607ca6c444bf0f929f28b0
cipher24 <mediumAndrew@yandex.ru>
2023-11-09 02:17:41 +0300

    feat: added header, footer and icons
    добавлены начальные версии компонентов header и footer, так же были добавлени иконки логотипа
    приложения. В черновом варианте подключил шрифт в head
    
    Tow-track-frontend #8

---
+++ src/shared/ui/icons/index.js
@@ -0,0 +1,1 @@
+// import { ExpressLogo } from './express-logo';
---
+++ src/widget/footer/footer.stories.js
@@ -0,0 +1,12 @@
+import Footer from './footer';
+
+export default {
+	title: 'widget/Footer',
+	component: Footer,
+	tags: ['autodocs'],
+	arg: {
+		primary: true,
+	},
+};
+
+export const Default = {};
