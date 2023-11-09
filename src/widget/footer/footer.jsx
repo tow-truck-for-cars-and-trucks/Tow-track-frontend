9dfa37a8a2841b2e2b77ef4760852660af54a7fe
cipher24 <mediumAndrew@yandex.ru>
2023-11-10 02:49:21 +0300

    refactor: added BEM to footer
    Tow-track-frontend #8

--- src/widget/footer/footer.jsx
+++ src/widget/footer/footer.jsx
@@ -1,31 +1,45 @@
 import './footer.scss';
 import ExpressLogo from '../../shared/ui/icons/express-logo';
+import Logo from '../../shared/ui/icons/logo';
 
 function Footer() {
 	return (
-		<footer>
-			<div className="container">
-				<ExpressLogo width="158px" height="70px" />
-				<div className="main">
-					<div className="phone">
-						<span className="phone-span">8 880 111 2222</span>
-						<span>Круглосуточно</span>
+		<footer className="footer">
+			<div className="footer__container">
+				<div className="footer__main">
+					<div className="footer__logo">
+						<Logo width="100px" height="100px" />
 					</div>
-					<span className="contacts-span">Контакты</span>
-				</div>
-				<div className="documents">
-					<div className="offer">
-						<span>Оферта</span>
-						<span>Политика конфидициальности</span>
+					<div className="footer__column">
+						<ExpressLogo width="158px" height="70px" />
+						<div className="footer__main-contacts">
+							<div className="footer__phone-block">
+								<span className="footer__phone-number">8 880 111 2222</span>
+								<div>Круглосуточно</div>
+							</div>
+							<div className="footer__contacts">Контакты</div>
+						</div>
+						<div className="footer__documents">
+							<a href="/" className="footer__link">
+								Оферта
+							</a>
+							<a href="/" className="footer__link">
+								Политика конфидициальности
+							</a>
+							<a href="/" className="footer__link">
+								Пользовательское соглашение
+							</a>
+							<a href="/" className="footer__link">
+								Сертификация эвакуаторов
+							</a>
+						</div>
 					</div>
-					<span>Пользовавтельское соглашение</span>
-					<span>Сертификация эвакуаторов</span>
 				</div>
-				<div className="copyright">
-					<span>
+				<div className="footer__copyright">
+					<div>
 						Информация, представленная на сайте, не является публичной офертой
-					</span>
-					<span>&#169; 2023 TT Express</span>
+					</div>
+					<div>&copy; 2023 TT Express</div>
 				</div>
 			</div>
 		</footer>
