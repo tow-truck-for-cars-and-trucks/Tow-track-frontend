705c2bdddffe088617b46968a5f43e0a11e98c9c
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-11-15 18:26:41 +0300

    feat: contacts-map have been added

---
+++ src/entities/contacts-map/contacts-map.jsx
@@ -0,0 +1,24 @@
+import './contacts-map.scss';
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import ContactsInfo from '../../shared/ui/contacts-info/contacts-info';
+
+function ContactsMap() {
+	const defaultState = {
+		center: [55.588867, 37.031152],
+		zoom: 8,
+	};
+	return (
+		<section className="contacts-map">
+			<div className="contacts-map__container">
+				<YMaps>
+					<Map width="358px" height="224px" defaultState={defaultState}>
+						<Placemark geometry={[55.588867, 37.031152]} />
+					</Map>
+				</YMaps>
+			</div>
+			<ContactsInfo />
+		</section>
+	);
+}
+
+export default ContactsMap;
