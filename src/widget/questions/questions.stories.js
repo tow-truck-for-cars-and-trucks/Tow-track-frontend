673117810de218274219b6db6e56c46eed814780
Kostya Zelenin <constantine.green80@yandex.ru>
2023-11-16 17:33:05 +0100

    feat: add questions + icons icPlus, icMunus

---
+++ src/widget/questions/questions.stories.js
@@ -0,0 +1,12 @@
+import Questions from './questions';
+
+export default {
+	title: 'widget/Questions',
+	component: Questions,
+	tags: ['autodocs'],
+	arg: {
+		primary: true,
+	},
+};
+
+export const Default = {};
