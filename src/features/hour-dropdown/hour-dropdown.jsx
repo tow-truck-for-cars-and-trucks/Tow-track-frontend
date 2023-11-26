9853a5605b923bf365d688238426a5a9bac42332
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-11-26 21:07:41 +0300

    feat: defedded-order popup has been added

--- src/features/hour-dropdown/hour-dropdown.jsx
+++ src/features/hour-dropdown/hour-dropdown.jsx
@@ -1,18 +1,22 @@
 import Dropdown from '../../shared/ui/dropdown/dropdown';
 
-function HourDropdown({ value, onChange }) {
+function HourDropdown({ value, onChange, startHour }) {
   return (
     <Dropdown
       value={value}
-      onChange={() => onChange()}
-      options={Array(24)
+      onChange={onChange}
+      options={Array(24 - startHour)
         .fill()
         .map((_value, index) => ({
-          id: index,
-          label: index.toString().padStart(2, '0'),
+          id: index + startHour,
+          label: (index + startHour).toString().padStart(2, '0'),
         }))}
     />
   );
 }
+
+HourDropdown.defaultProps = {
+  startHour: 0,
+};
 
 export default HourDropdown;
--- src/features/minute-dropdown/minute-dropdown.jsx
+++ src/features/minute-dropdown/minute-dropdown.jsx
@@ -1,18 +1,22 @@
 import Dropdown from '../../shared/ui/dropdown/dropdown';
 
-function MinuteDropdown({ value, onChange }) {
+function MinuteDropdown({ value, onChange, startMinute }) {
   return (
     <Dropdown
       value={value}
       onChange={onChange}
-      options={Array(6)
+      options={Array(6 - Math.floor(startMinute / 10))
         .fill()
         .map((_value, index) => ({
-          id: index * 10,
-          label: index.toString().padEnd(2, '0'),
+          id: index * 10 + startMinute,
+          label: (index * 10 + startMinute).toString().padEnd(2, '0'),
         }))}
     />
   );
 }
+
+MinuteDropdown.defaultProps = {
+  startMinute: 0,
+};
 
 export default MinuteDropdown;
