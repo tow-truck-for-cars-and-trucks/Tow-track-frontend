e7555847ddf44a84829b9d38cee2f3473269c099
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-12-07 16:22:39 +0300

    refactor: total price added in store

--- src/app/model/store.js
+++ src/app/model/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tariffSlice from '../../features/create-order/model/tariff-slice';
 import carTypeSlice from '../../features/create-order/model/car-type-slice';
+import orderPriceSlice from '../../features/create-order/model/total-price';
 
 const store = configureStore({
   reducer: {
     allPricing: tariffSlice,
     allCars: carTypeSlice,
+    totalPrice: orderPriceSlice,
   },
 });
 
