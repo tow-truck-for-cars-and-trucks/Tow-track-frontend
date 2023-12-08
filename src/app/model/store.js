7b518691e23f3501a5cbe2e2e132c17f9688135b
Sengeer <sengeer@mail.ru>
2023-12-08 22:04:50 +0300

    feat: add a request for feedback to the store

--- src/app/model/store.js
+++ src/app/model/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import tariffSlice from '../../features/create-order/model/tariff-slice';
 import carTypeSlice from '../../features/create-order/model/car-type-slice';
 import orderPriceSlice from '../../features/create-order/model/total-price';
+import feedbacksSlice from '../../features/create-order/model/feedback-slice';
 
 const store = configureStore({
   reducer: {
     allPricing: tariffSlice,
     allCars: carTypeSlice,
     totalPrice: orderPriceSlice,
+    allFeedbacks: feedbacksSlice,
   },
 });
 
