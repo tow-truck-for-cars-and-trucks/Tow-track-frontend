10058e8aa6dea7c11582f2f296a247ae1bbc6509
Sengeer <sengeer@mail.ru>
2023-12-17 13:32:29 +0300

    feat: implement price-preloader

---
+++ src/shared/ui/price-preloader/price-preloader.jsx
@@ -0,0 +1,13 @@
+import './price-preloader.scss';
+
+function PricePreloader() {
+  return (
+    <div className="price-preloader">
+      <span className="price-preloader__cell" />
+      <span className="price-preloader__cell" />
+      <span className="price-preloader__cell" />
+    </div>
+  );
+}
+
+export default PricePreloader;
