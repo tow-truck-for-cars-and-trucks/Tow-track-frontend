b0b83244d1ef04dbcf65066ef9c28a2c81322d9d
Lopakadude <lopakadude@gmail.com>
2023-12-18 00:42:48 +0300

    feat: 404

---
+++ src/widget/not-found/not-found.stories.js
@@ -0,0 +1,12 @@
+import NotFound from './not-found';
+
+export default {
+  title: 'widget/MyOrder',
+  component: NotFound,
+  tags: ['autodocs'],
+  arg: {
+    primary: true,
+  },
+};
+
+export const Default = {};
