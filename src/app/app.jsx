9cedf312c59a5c1a4748ce2878bba21855b3fad1
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-12-26 15:48:07 +0300

    feat:lazy loading have been added

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 import { getPlan } from '../features/create-order/model/plan-slice';
 import { getCarType } from '../features/create-order/model/car-type-slice';
 import Main from '../pages/main/main';
-import ContactsPage from '../pages/contacts-page/contacts-page';
-import Order from '../pages/order/order';
-import RegisterPage from '../pages/register-page/register-page';
-import SuccessOrderPage from '../pages/success-order-page/success-order-page';
-import MyOrderPage from '../pages/my-order-page/my-order-page';
-import ProtectedRoute from './router/ProtectedRoute';
+import Preloader from '../shared/ui/preloader/preloader';
+
+const ContactsPage = lazy(() => import('../pages/contacts-page/contacts-page'));
+const Order = lazy(() => import('../pages/order/order'));
+const RegisterPage = lazy(() => import('../pages/register-page/register-page'));
+const SuccessOrderPage = lazy(() =>
+  import('../pages/success-order-page/success-order-page')
+);
+const MyOrderPage = lazy(() => import('../pages/my-order-page/my-order-page'));
+const ProtectedRoute = lazy(() => import('./router/ProtectedRoute'));
 
 function App() {
   const dispatch = useDispatch();
@@ -22,32 +26,74 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/contacts" element={<ContactsPage />} />
+      <Route
+        path="/contacts"
+        element={
+          <Suspense fallback={<Preloader />}>
+            <ContactsPage />
+          </Suspense>
+        }
+      />
       <Route
         path="/order/:id"
-        element={<ProtectedRoute forLoggedUser element={<Order />} />}
+        element={
+          <ProtectedRoute
+            forLoggedUser
+            element={
+              <Suspense fallback={<Preloader />}>
+                <Order />
+              </Suspense>
+            }
+          />
+        }
       />
       <Route
         path="/success-order/:id"
         element={
-          <ProtectedRoute forLoggedUser element={<SuccessOrderPage />} />
+          <ProtectedRoute
+            forLoggedUser
+            element={
+              <Suspense fallback={<Preloader />}>
+                <SuccessOrderPage />
+              </Suspense>
+            }
+          />
         }
       />
       <Route
         path="/register"
-        element={<ProtectedRoute element={<RegisterPage />} />}
+        element={
+          <ProtectedRoute
+            element={
+              <Suspense fallback={<Preloader />}>
+                <RegisterPage />
+              </Suspense>
+            }
+          />
+        }
       />
       <Route
         path="/my-orders"
-        element={<ProtectedRoute forLoggedUser element={<MyOrderPage />} />}
+        element={
+          <ProtectedRoute
+            forLoggedUser
+            element={
+              <Suspense fallback={<Preloader />}>
+                <MyOrderPage />
+              </Suspense>
+            }
+          />
+        }
       />
       <Route
         path="/profile"
         element={
-          <ProtectedRoute
-            forLoggedUser
-            element={<h1>Эта страница в разработке</h1>}
-          />
+          <Suspense fallback={<Preloader />}>
+            <ProtectedRoute
+              forLoggedUser
+              element={<h1>Эта страница в разработке</h1>}
+            />
+          </Suspense>
         }
       />
     </Routes>
